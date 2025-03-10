3ceebfa4c0739fc75ae13a0db74ba6b380538275
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-03-10 10:38:17 +0500

    add Image component to TopBar: include search icon in the search form

--- frontend/src/components/topbar/Topbar.jsx
+++ frontend/src/components/topbar/Topbar.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import "./topBar.css";
 import UserButton from "../userButton/UserButton";
+import Image from "../images/Image";
 
 const TopBar = () => {
   const navigate = useNavigate();
@@ -12,6 +13,7 @@ const TopBar = () => {
   return (
     <div className="topBar">
       <form onSubmit={handleSubmit} className="search">
+        <Image path="/general/search.svg" alt="" />
         <input type="text" placeholder="Search" />
       </form>
       <UserButton />
