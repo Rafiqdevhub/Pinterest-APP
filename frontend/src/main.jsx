ae7542e45b9750ac94a4ba88d35ebfce323f29a2
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-03-10 10:35:25 +0500

    add react-query support: wrap application in QueryClientProvider for data fetching

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import MainLayout from "./routes/layout/MainLayout.jsx";
 
@@ -13,19 +14,23 @@ const ProfilePage = React.lazy(() =>
 const SearchPage = React.lazy(() => import("./routes/searchPage/searchPage"));
 const AuthPage = React.lazy(() => import("./routes/authPage/AuthPage"));
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/create" element={<CreatePage />} />
-          <Route path="/pin/:id" element={<PostPage />} />
-          <Route path="/:username" element={<ProfilePage />} />
-          <Route path="/search" element={<SearchPage />} />
-        </Route>
-        <Route path="/auth" element={<AuthPage />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/create" element={<CreatePage />} />
+            <Route path="/pin/:id" element={<PostPage />} />
+            <Route path="/:username" element={<ProfilePage />} />
+            <Route path="/search" element={<SearchPage />} />
+          </Route>
+          <Route path="/auth" element={<AuthPage />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
