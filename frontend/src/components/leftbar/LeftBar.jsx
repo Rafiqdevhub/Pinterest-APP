99f39a42c4843188dae3529b59f626700a6dcde5
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-03-11 09:38:11 +0500

    refactor LeftBar component: simplify menu item rendering with a map function and enhance accessibility with aria-labels

--- frontend/src/components/leftbar/LeftBar.jsx
+++ frontend/src/components/leftbar/LeftBar.jsx
@@ -1,32 +1,40 @@
-import { Link } from "react-router";
+import { memo } from "react";
+import { Link } from "react-router-dom";
 import "./leftBar.css";
 import Image from "../images/Image";
 
+const menuItems = [
+  { path: "/", icon: "/general/logo.png", alt: "Pinterest Logo", isLogo: true },
+  { path: "/", icon: "/general/home.svg", alt: "Home" },
+  { path: "/create", icon: "/general/create.svg", alt: "Create" },
+  { path: "/", icon: "/general/updates.svg", alt: "Updates" },
+  { path: "/", icon: "/general/messages.svg", alt: "Messages" },
+];
+
 const LeftBar = () => {
   return (
-    <div className="leftBar">
+    <nav className="leftBar" aria-label="Main navigation">
       <div className="menuIcons">
-        <Link to="/" className="menuIcon">
-          <Image path="/general/logo.png" alt="" className="logo" />
-        </Link>
-        <Link to="/" className="menuIcon">
-          <Image path="/general/home.svg" alt="" />
-        </Link>
-        <Link to="/create" className="menuIcon">
-          <Image path="/general/create.svg" alt="" />
-        </Link>
-        <Link to="/" className="menuIcon">
-          <Image path="/general/updates.svg" alt="" />
-        </Link>
-        <Link to="/" className="menuIcon">
-          <Image path="/general/messages.svg" alt="" />
-        </Link>
+        {menuItems.map((item) => (
+          <Link
+            key={item.icon}
+            to={item.path}
+            className="menuIcon"
+            aria-label={item.alt}
+          >
+            <Image
+              path={item.icon}
+              alt={item.alt}
+              className={item.isLogo ? "logo" : ""}
+            />
+          </Link>
+        ))}
       </div>
-      <Link to="/" className="menuIcon">
-        <Image path="/general/settings.svg" alt="" />
+      <Link to="/settings" className="menuIcon" aria-label="Settings">
+        <Image path="/general/settings.svg" alt="Settings" />
       </Link>
-    </div>
+    </nav>
   );
 };
 
-export default LeftBar;
+export default memo(LeftBar);
